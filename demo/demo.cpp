04298dbc2216333f205350380d23de2e11a1b764
mattvchandler <tardarsauce@gmail.com>
2017-06-23 11:59:26 -0800

    open an SFML window

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 
+#include <SFML/Window.hpp>
+#include <SFML/OpenGL.hpp>
+#include <SFML/System.hpp>
+
+
 int main()
 {
-    std::cout<<"Hello, world!"<<std::endl;
+    sf::Window win(sf::VideoMode(1024, 786), "Textogl Demo", sf::Style::Default, sf::ContextSettings(32, 8, 0, 4, 0));
+    win.setKeyRepeatEnabled(false);
+
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+    sf::Event ev;
+
+    bool running = true;
+    while(running)
+    {
+        while(win.pollEvent(ev))
+        {
+            switch(ev.type)
+            {
+                // close window
+                case sf::Event::Closed:
+                    running = false;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+
+    win.close();
+
+    return EXIT_SUCCESS;
 }
